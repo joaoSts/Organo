9c0fe5633613955c98749129e917a31fd61e6774
João Mendonça <85750169+joaoSts@users.noreply.github.com>
2023-06-01 15:05:32 -0300

    Update Banner.js

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -5,7 +5,7 @@ function Banner(){
     //jsx
     return (
         <header className='Banner' width={"100%"}>
-        <img src=".../imagem/Banner.png" alt="O Banner principal da página" width={"100%"}/>
+        <img src="..public/imagem/Banner.png" alt="O Banner principal da página" width={"100%"}/>
         </header>
     
     )
